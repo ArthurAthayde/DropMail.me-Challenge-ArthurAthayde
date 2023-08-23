614ec82d715fbc857a185a04240a065cd044a61c
ArthurAthayde <arthur.athaydee@gmail.com>
2023-08-23 16:09:38 -0300

    feat: botão para permitir notificação finalizado // header finalizado

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,11 +3,33 @@ import logotipo from "../../assets/logotipo.png";
 import "./style.css";
 
 export const Navbar = () => {
+  const [notificationPermission, setNotificationPermission] = useState(
+    window.Notification.permission
+  );
+
+  const allowNotify = () => {
+    if (notificationPermission !== "granted") {
+      window.Notification.requestPermission((res) => {
+        if (res === "granted") {
+          setNotificationPermission("granted");
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    setNotificationPermission(window.Notification.permission);
+  }, []);
+
   return (
     <>
       <header className="navHeader">
         <img src={logotipo} alt="Logotipo" className="logotipo" />
-        <button className="notifyBtn">Enable notifications!</button>
+        <button onClick={allowNotify} className="notifyBtn">
+          {notificationPermission !== "granted"
+            ? "Enable notifications!"
+            : "Notifications allowed"}
+        </button>
       </header>
     </>
   );
