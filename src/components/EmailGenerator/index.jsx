dbcebecf66904a07f78ab13e789cb26d7c665517
ArthurAthayde <arthur.athaydee@gmail.com>
2023-08-23 23:26:58 -0300

    feat: introdução do tailwind

--- src/components/EmailGenerator/index.jsx
+++ src/components/EmailGenerator/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./style.css";
 import { useQuery, gql, useMutation } from "@apollo/client";
 import { EXAMPLE, NEW_SESSION } from "../../Apollo/querys";
 import { emailDatabase } from "../../database";
@@ -22,15 +21,28 @@ export const EmailGenerator = () => {
 
   return (
     <>
-      <section className="generator_container">
-        <div className="generator_controler">
+      <section className="flex items-center justify-center py-5 w-screen h-32">
+        <div className="flex flex-col gap-2 w-full max-w-md">
           <span>Your temporary email address</span>
-          <div className="input_controler" onClick={() => copyEmail()}>
-            <input type="text" disabled defaultValue={emailDatabase.toAddr} />
-            <img src={copyIcon} alt="copy icon" className="copyIcon" />
-            <button>Copy</button>
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={() => copyEmail()}
+          >
+            <input
+              type="text"
+              disabled
+              defaultValue={emailDatabase.toAddr}
+              className="max-w-[450px] w-3/4 p-2"
+            />
+            <img
+              src={copyIcon}
+              alt="copy icon"
+              className="h-6 w-6 bg-center"
+              style={{ height: "20px" }}
+            />
+            <button className="btn btn-primary">Copy</button>
           </div>
-          <div className="refresh_container">
+          <div className="flex gap-1 items-end self-center">
             <span>Autorefresh in</span>
             <Countdown />
           </div>
